ec87e72d42de59b4ded9b2f380c67f147e4e8e63
Brian <baustin27@gmail.com>
2018-04-06 10:14:56 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+**Work in Progress starting April 6, 2018**
+
+An app built in ReactJS to direct humans to the top 30 activities full of radness in San Diego, CA
+
+To get it to work: run 'npm install' in command prompt of your choice
+
+'npm start' will allow viewing locally 
---
+++ src/app.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        
+        <div className='container'>
+          <div className='jumbotron jumbotron-fluid padding-top-4'>
+            <h1 className='display-4'>San Diego Top Spots</h1>
+            <p className='lead'>A list of the top 30 places to see in San Diego, California</p>
+          </div>
+
+        </div>
+ 
+      
+      
+
+
+
+
+
+      </div>  
+    );
+  }
+}
+
+export default App;
